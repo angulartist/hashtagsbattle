dba2deb5a4541efa06b0554e174b11f86fe039ca
techlead <belabesmohammed@gmail.com>
2019-06-25 20:48:23 +0200

    preparing redis+local testing

--- app-engine-express/server/app.js
+++ app-engine-express/server/app.js
@@ -2,6 +2,8 @@ const bodyParser = require('body-parser')
 const app = require('express')()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
+const cache = require('memory-cache')
+
 
 io.on('connection', socket => {
     io.emit('connected')
@@ -43,5 +45,52 @@ app.post('/push', (req, res) => {
     res.status(204).send()
 })
 
+// # LOCAL TESTING ONLY # //
+
+const {PubSub} = require(`@google-cloud/pubsub`)
+
+const pubsub = new PubSub()
+
+const subscriptionName = 'projects/notbanana-7f869/subscriptions/new_tweets'
+const timeout = 60
+
+const subscription = pubsub.subscription(subscriptionName)
+
+let messageCount = 0
+
+/**
+ * Handler for received message.
+ * @param {Object} message
+ */
+const messageHandler = message => {
+    // console.log(`Received message ${message.id}:`)
+    // console.log(`Data: ${message.data}`)
+    // console.log(`tAttributes: ${message.attributes}`)
+    messageCount += 1
+
+    cache.put('count', messageCount)
+    // Ack the messae
+    message.ack()
+}
+
+setInterval(() => {
+    console.log(cache.get('count'))
+    messageCount = 0
+}, 1 * 60 * 1000)
+
+const point = {
+  "type": "Feature",
+  "geometry": {
+    "type": "Point",
+    "coordinates": [125.6, 10.1]
+  },
+  "properties": {
+    "name": "Dinagat Islands"
+  }
+}
+
+
+// Listen for new messages until timeout is hit
+subscription.on(`message`, messageHandler)
 
 module.exports = http
